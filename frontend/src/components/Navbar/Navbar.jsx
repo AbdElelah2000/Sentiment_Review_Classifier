994c1567b377df00394f7d40cc915255af353356
AbdElelah <arafaha@mcmaster.ca>
2023-06-29 23:33:45 -0400

    initial frontend design

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import './navbar.css'
+import Logo from '../../assets/logo_trans.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='Navbar-container'>
+      <div className='Navbar-logo'>
+        <img className='Navbar-logo-img'src={Logo} alt="logo" />
+      </div>
+    </div>
   )
 }
 
---
+++ frontend/src/containers/Mainpage/Mainpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './mainpage.css'
+import Form from '../../components/Form/Form'
+import Button from '../../components/Button/Button'
+
+const Mainpage = () => {
+  return (
+    <div className='Mainpage-container'>
+        <Form />
+        <Button />
+    </div>
+  )
+}
+
+export default Mainpage
